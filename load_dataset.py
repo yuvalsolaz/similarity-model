0c7ca68f17e7b2f8fc73c0322a30939d33fb85f7
yuval.solaz <yuval.solaz@gmail.com>
2023-09-02 16:21:32 +0300

    clean

--- load_dataset.py
+++ load_dataset.py
@@ -1,15 +1,4 @@
 
-'''
-collect all text files in given directory with metadata fields
-cmd ine
-find . -type f -exec file {} \; | grep ":.* ASCII text"
-
-if we want to find all text files in the current directory, including its sub-directories, then we have to augment
-the command using the Linux find command:
-find . -type f -exec file -i {} \; | grep " text/plain;" | wc
-
-'''
-
 import torch
 from datasets import load_dataset
 import torchvision.transforms as T
@@ -17,43 +6,6 @@ from transformers import AutoFeatureExtractor, AutoModel
 from tensorboard_handler import write_embedding_preview
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-'''
-    load images and labels from disk 
-'''
-#
-# def load_images(root_directory):
-#     target_size = (512, 512)
-#     # Define transformations to apply to the images (resizing, normalization, etc.)
-#     transform = transforms.Compose([
-#         transforms.Resize(target_size),  # Resize images to target size pixels
-#         transforms.ToTensor(),           # Convert images to PyTorch tensors
-#         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize using ImageNet statistics
-#     ])
-#
-#     # Create an ImageFolder dataset
-#     return ImageFolder(root=root_directory, transform=transform)
-
-#
-# def add_embeddings(dataset:Dataset):
-#     model_checkpoint = 'facebook/convnext-tiny-224'
-#     print(f'loading model from : {model_checkpoint}')
-#     image_processor = AutoImageProcessor.from_pretrained(model_checkpoint)
-#     model = ConvNextModel.from_pretrained(model_checkpoint)
-#     # print(f'copy model to {device} device...')
-#     # model.to_device(device)
-#
-#     def get_embeddings(image):
-#         inputs = image_processor(image, return_tensors="pt")
-#         with torch.no_grad():
-#             outputs = model(**inputs)
-#             return outputs.last_hidden_state
-#
-#     image_embedding_field = 'image_embedding'
-#     print(f'map image embeddings to {image_embedding_field}')
-#     dataset = dataset.map(lambda x: {image_embedding_field: get_embeddings(x).detach().cpu().numpy()[0]})
-#     return dataset
-
 
 if __name__ == '__main__':
     # TODO parameters
@@ -91,31 +43,19 @@ if __name__ == '__main__':
             with torch.no_grad():
                 embeddings = model(**new_batch).pooler_output.cpu()
             # add image path
-            return {"image_path": image_path, "label_name":label_names, "embeddings": embeddings}
+            return {"image_path": image_path, "label_name": label_names, "embeddings": embeddings}
 
         return pp
+
 
     # Here, we map embedding extraction utility on our subset of candidate images.
     model = AutoModel.from_pretrained(model_checkpoint)
     batch_size = 24
     extract_fn = extract_embeddings(model.to(device))
     dataset_emb = dataset.map(extract_fn, batched=True, batch_size=24)
-    print(f'embeddings dataset created with {dataset_emb.shape} samples...' )
+    print(f'embeddings dataset created with {dataset_emb.shape} samples...')
 
     log_dir = f'embeddings/{model_checkpoint}'
-    print(f'write tensorboard logs to {log_dir}' )
+    print(f'write tensorboard logs to {log_dir}')
     write_embedding_preview(log_dir=log_dir, ds=dataset_emb['train'], embedding_field='embeddings', tag='vision')
     print(f'run: tensorboard --logdir {log_dir} --bind_all')
-
-
-
-
-
-
-
-
-
-
-
-
-
