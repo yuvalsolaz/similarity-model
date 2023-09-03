d608e09d235b8fdc1a8c55acaade8ebffbb4a944
yuval solaz <yuval.solaz@gmail.com>
2023-09-03 17:30:09 +0300

    search_engine

--- search_engine.py
+++ search_engine.py
@@ -25,11 +25,11 @@ class SearchEngine(object):
         self._transformation_chain = T.Compose(
             [
                 # We first resize the input image to 256x256 and then we take center crop.
-                T.Resize(int((256 / 224) * extractor.size["shortest_edge"])),
-                T.CenterCrop(extractor.size["shortest_edge"]),
+                T.Resize(int((256 / 224) * self._extractor.size["shortest_edge"])),
+                T.CenterCrop(self._extractor.size["shortest_edge"]),
                 T.ToTensor(),
                 T.Lambda(lambda x: torch.cat([x, x, x], 0)),
-                T.Normalize(mean=extractor.image_mean, std=extractor.image_std),
+                T.Normalize(mean=self._extractor.image_mean, std=self._extractor.image_std),
             ]
         )
 
@@ -37,20 +37,38 @@ class SearchEngine(object):
         self._model.to(device)
 
         print(f'load search dataset from {dataset_path}')
-        self._dataset = load_dataset('imagefolder', data_dir=dataset_path, drop_labels=False)
-        self._label_ids2label_names = self._dataset['train'].features['label'].names
+        self._dataset = load_dataset('imagefolder', data_dir=dataset_path, drop_labels=False)['train']
+        self._label_ids2label_names = self._dataset.features['label'].names
+
+        extract_fn = self.__batch_embeddings(self._model.to(device))
+        self._dataset = self._dataset.map(extract_fn, batched=True, batch_size=24)
+        print(f'embeddings dataset created with {self._dataset.shape} samples...')
 
         print(f'create faiss index on {self._index_column}...')
         self._dataset.add_faiss_index(column=self._index_column, metric_type=metric_type)
 
-    def __extract_embeddings(self, image):
-        device = self.__model.device
-        image_transformed = transformation_chain(image)
-        new_image = {"pixel_values": image_transformed.to(device)}
-        with torch.no_grad():
-            # embeddings = model(**new_batch).pooler_output.cpu()
-            embeddings = self._model.get_image_features(**new_image).cpu()
-        return {"embeddings": embeddings}
+    # map embedding on dataset images.
+    def __batch_embeddings(self, model: torch.nn.Module):
+        """Utility to compute embeddings."""
+        device = model.device
+
+        def pp(batch):
+            images = batch["image"]
+            image_path = [image.filename for image in images]
+            labels = batch["label"]
+            label_names = [self._label_ids2label_names[label] for label in labels]
+            image_batch_transformed = torch.stack(
+                [self._transformation_chain(image) for image in images]
+            )
+            new_batch = {"pixel_values": image_batch_transformed.to(device)}
+            with torch.no_grad():
+                # embeddings = model(**new_batch).pooler_output.cpu()
+                embeddings = model.get_image_features(**new_batch).cpu()
+
+            # add image path
+            return {"image_path": image_path, "label_name": label_names, "embeddings": embeddings}
+
+        return pp
 
     def get_embeddings(self, image):
         return self.__extract_embeddings(image)
