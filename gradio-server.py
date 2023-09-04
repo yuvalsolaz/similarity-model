e1cb8c316b8b50b26a194fb8b0d20856e1683f71
yuval solaz <yuval.solaz@gmail.com>
2023-09-04 18:05:29 +0300

    topk

--- gradio-server.py
+++ gradio-server.py
@@ -1,5 +1,4 @@
 import gradio as gr
-import torch
 import faiss
 
 from search_engine import SearchEngine
@@ -17,8 +16,7 @@ title = "search similar documents"
 description = 'Select document for search'
 
 
-def run_query(input_image):
-    top_k = 5
+def run_query(input_image, top_k):
     scores, images = search_engine.search(query_image=input_image, k=top_k)
     results = []
     for i in range(len(scores)):
@@ -30,13 +28,12 @@ def run_query(input_image):
 
 if __name__ == '__main__':
     inputs_image = [
-        gr.Image(
-            type='filepath',
-            label='Input Image'
-        )
+        gr.Image(type='filepath', label='Input Image'),
+        gr.Slider(label='top_k', show_label=True, value=5, minimum=1, maximum=10, step=1)
     ]
+
     outputs_image = [
-        gr.Gallery(label='Search Results', ).style(columns=[3], rowa=[3], object_fit='contain', height='auto')]
+        gr.Gallery(label='Search Results').style(columns=[3], rowa=[3], object_fit='contain', height='auto')]
     demo = gr.Interface(
         fn=run_query,
         inputs=inputs_image,
@@ -44,22 +41,4 @@ if __name__ == '__main__':
         title=title,
         description=description,
     )
-
     demo.launch(server_name='0.0.0.0')
-
-# with gr.Blocks() as demo:
-#     with gr.Row():
-#         gr.Markdown('query document')
-#         inputs_image = [
-#             gr.Image(
-#                 type='filepath',
-#                 label='Input Image'
-#             )
-#         ]
-#         search_btn = gr.Button('search')
-#         top_k_input = gr.Slider(label='top_k', show_label=True, value=5, minimum=1, maximum=10, step=1)
-#     with gr.Column():
-#         gr.Markdown('results documents')
-#         search_inputs = [inputs_image, top_k_input]
-#         results_output = gr.Gallery(columns=[3], rows=[3], height='auto')
-#         search_btn.click(fn=run_query, inputs=search_inputs, outputs=results_output)
